1e0aff866a82d8503f0d9763dd158ec5f104ef2a
Owen Jones <owenjones@Owens-Air.lan>
2021-04-11 19:03:58 -0400

    sdf

--- tasks.py
+++ tasks.py
@@ -5,6 +5,7 @@ import os
 celery_app=Celery('tasks',backend='redis://:p1cfadf555e1f6b0a0fcd1f54f8554aebe4ce57b086bf86bb05eb15d343ceebd0@ec2-35-169-115-180.compute-1.amazonaws.com:9969',broker='redis://:p1cfadf555e1f6b0a0fcd1f54f8554aebe4ce57b086bf86bb05eb15d343ceebd0@ec2-35-169-115-180.compute-1.amazonaws.com:9969')
 celery_app.conf.update(BROKER_URL=os.environ['REDIS_URL'],
                 CELERY_RESULT_BACKEND=os.environ['REDIS_URL'])
+print("running celery app")
 @celery_app.task
 def generate_machine_move(fen):
     print("i am in this task")
