e18723a063f19c3feeba47270c64ec0b271005ff
Owen Jones <owenjones@Owens-Air.lan>
2021-04-11 18:50:16 -0400

    new attempt

--- tasks.py
+++ tasks.py
@@ -1,7 +1,7 @@
 from celery import Celery
 import ai_model
 import chess
-celery_app=Celery('tasks',backend='redis://:p1cfadf555e1f6b0a0fcd1f54f8554aebe4ce57b086bf86bb05eb15d343ceebd0@ec2-35-169-115-180.compute-1.amazonaws.com:9969',broker='redis://:p1cfadf555e1f6b0a0fcd1f54f8554aebe4ce57b086bf86bb05eb15d343ceebd0@ec2-35-169-115-180.compute-1.amazonaws.com:9969')
+celery_app=Celery('tasks',backend='redis://:**@ec2-35-169-115-180.compute-1.amazonaws.com:9969/',broker='redis://:p1cfadf555e1f6b0a0fcd1f54f8554aebe4ce57b086bf86bb05eb15d343ceebd0@ec2-35-169-115-180.compute-1.amazonaws.com:9969')
 
 @celery_app.task
 def generate_machine_move(fen):
