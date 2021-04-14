5f5f8dbeaaafc220c32aa6d5a57750586a18d800
Owen Jones <owenjones@Owens-Air.lan>
2021-04-14 11:39:08 -0400

    removed weird comment i dont remember making

--- tasks.py
+++ tasks.py
@@ -3,8 +3,8 @@ import ai_model
 import chess
 import os
 celery_app=Celery('tasks',backend='redis://:p1cfadf555e1f6b0a0fcd1f54f8554aebe4ce57b086bf86bb05eb15d343ceebd0@ec2-35-169-115-180.compute-1.amazonaws.com:9969',broker='redis://:p1cfadf555e1f6b0a0fcd1f54f8554aebe4ce57b086bf86bb05eb15d343ceebd0@ec2-35-169-115-180.compute-1.amazonaws.com:9969')
-#celery_app.conf.update(broker_pool_limit=None,broker_url=os.environ['REDIS_URL'],
-#                result_backend=os.environ['REDIS_URL'])
+celery_app.conf.update(broker_pool_limit=None,broker_url=os.environ['REDIS_URL'],
+                result_backend=os.environ['REDIS_URL'])
 @celery_app.task
 def generate_machine_move(fen):
     board=chess.Board()
