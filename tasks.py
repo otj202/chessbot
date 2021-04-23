0bda5ff348c10a6b597f6731ed45c6c69db25c01
Owen Jones <owenjones@Owens-Air.lan>
2021-04-23 12:13:47 -0400

    tried new REDIS URL

--- tasks.py
+++ tasks.py
@@ -2,8 +2,8 @@ from celery import Celery
 import ai_model
 import chess
 import os
-celery_app=Celery('tasks',backend='redis://:p1cfadf555e1f6b0a0fcd1f54f8554aebe4ce57b086bf86bb05eb15d343ceebd0@ec2-35-169-115-180.compute-1.amazonaws.com:9969',broker='redis://:p1cfadf555e1f6b0a0fcd1f54f8554aebe4ce57b086bf86bb05eb15d343ceebd0@ec2-35-169-115-180.compute-1.amazonaws.com:9969')
-celery_app.conf.update(broker_pool_limit=None,broker_url='redis://:p1cfadf555e1f6b0a0fcd1f54f8554aebe4ce57b086bf86bb05eb15d343ceebd0@ec2-35-169-115-180.compute-1.amazonaws.com:9969',result_backend='redis://:p1cfadf555e1f6b0a0fcd1f54f8554aebe4ce57b086bf86bb05eb15d343ceebd0@ec2-35-169-115-180.compute-1.amazonaws.com:9969')
+celery_app=Celery('tasks',backend='redis://:p1cfadf555e1f6b0a0fcd1f54f8554aebe4ce57b086bf86bb05eb15d343ceebd0@ec2-34-197-240-190.compute-1.amazonaws.com:14819',broker='redis://:p1cfadf555e1f6b0a0fcd1f54f8554aebe4ce57b086bf86bb05eb15d343ceebd0@ec2-34-197-240-190.compute-1.amazonaws.com:14819')
+celery_app.conf.update(broker_pool_limit=None,broker_url='redis://:p1cfadf555e1f6b0a0fcd1f54f8554aebe4ce57b086bf86bb05eb15d343ceebd0@ec2-34-197-240-190.compute-1.amazonaws.com:14819',result_backend='redis://:p1cfadf555e1f6b0a0fcd1f54f8554aebe4ce57b086bf86bb05eb15d343ceebd0@ec2-34-197-240-190.compute-1.amazonaws.com:14819')
 @celery_app.task
 def generate_machine_move(fen):
 	for i in range(10):
