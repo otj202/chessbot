34af8cec2e2a723b15a58449a552cb983fdd5239
Owen Jones <owenjones@Owens-Air.lan>
2021-04-23 12:19:13 -0400

    fixed indentation error

--- tasks.py
+++ tasks.py
@@ -6,7 +6,7 @@ celery_app=Celery('tasks',backend='redis://:p1cfadf555e1f6b0a0fcd1f54f8554aebe4c
 celery_app.conf.update(broker_pool_limit=None,broker_url='redis://:p1cfadf555e1f6b0a0fcd1f54f8554aebe4ce57b086bf86bb05eb15d343ceebd0@ec2-34-197-240-190.compute-1.amazonaws.com:14819',result_backend='redis://:p1cfadf555e1f6b0a0fcd1f54f8554aebe4ce57b086bf86bb05eb15d343ceebd0@ec2-34-197-240-190.compute-1.amazonaws.com:14819')
 @celery_app.task
 def generate_machine_move(fen):
-	board=chess.Board()
+    board=chess.Board()
     board.set_fen(fen)
     machine_move=ai_model.predict(board)
     board.push(machine_move)
