2da992cf2122f00845dd11ccd58641a73f8e3509
Owen Jones <owenjones@Owens-Air.lan>
2021-04-23 12:17:14 -0400

    reverted to actual task

--- tasks.py
+++ tasks.py
@@ -6,11 +6,8 @@ celery_app=Celery('tasks',backend='redis://:p1cfadf555e1f6b0a0fcd1f54f8554aebe4c
 celery_app.conf.update(broker_pool_limit=None,broker_url='redis://:p1cfadf555e1f6b0a0fcd1f54f8554aebe4ce57b086bf86bb05eb15d343ceebd0@ec2-34-197-240-190.compute-1.amazonaws.com:14819',result_backend='redis://:p1cfadf555e1f6b0a0fcd1f54f8554aebe4ce57b086bf86bb05eb15d343ceebd0@ec2-34-197-240-190.compute-1.amazonaws.com:14819')
 @celery_app.task
 def generate_machine_move(fen):
-	for i in range(10):
-		print("generate machine move",i)
-	return "bar"
-	#board=chess.Board()
-    #board.set_fen(fen)
-    #machine_move=ai_model.predict(board)
-    #board.push(machine_move)
-    #return board.fen()
+	board=chess.Board()
+    board.set_fen(fen)
+    machine_move=ai_model.predict(board)
+    board.push(machine_move)
+    return board.fen()
