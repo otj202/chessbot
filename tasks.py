7832650a003f31b231354766d650266fd779734e
Owen Jones <otj202@nyu.edu>
2021-10-17 13:16:31 -0400

    dont try to use ssl

--- tasks.py
+++ tasks.py
@@ -2,8 +2,9 @@ from celery import Celery
 import ai_model
 import chess
 import os
+import ssl
 REDIS_URL=os.getenv("REDIS_URL")
-celery_app=Celery('tasks',backend=REDIS_URL,broker=REDIS_URL,ssl_cert_reqs=None)
+celery_app=Celery('tasks',backend=REDIS_URL,broker=REDIS_URL)
 celery_app.conf.update(broker_pool_limit=None,broker_url=REDIS_URL,result_backend=REDIS_URL,ssl_cert_reqs=None)
 @celery_app.task
 def generate_machine_move(fen):
