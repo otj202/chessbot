c4414b8a52538cef9ea5757e41706df0d7967372
Owen Jones <otj202@nyu.edu>
2021-10-17 17:17:23 -0400

    debugging

--- tasks.py
+++ tasks.py
@@ -4,10 +4,13 @@ import chess
 import os
 import ssl
 REDIS_URL=os.getenv("REDIS_URL") + "?ssl_cert_reqs=none"
-print("redis url is ",REDIS_URL)
-#celery_app=Celery('tasks',backend=REDIS_URL,broker=REDIS_URL)
-#celery_app.conf.update(broker_pool_limit=None,broker_url=REDIS_URL,result_backend=REDIS_URL)
-#@celery_app.task
+celery_app = None
+try:
+	celery_app=Celery('tasks',backend=REDIS_URL,broker=REDIS_URL)
+except:
+	print("/n/n/n/n/n url is ",REDIS_URL)
+celery_app.conf.update(broker_pool_limit=None,broker_url=REDIS_URL,result_backend=REDIS_URL)
+@celery_app.task
 def generate_machine_move(fen):
     board=chess.Board()
     board.set_fen(fen)
