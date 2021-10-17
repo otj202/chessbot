9b491168785029de86da7f4432e31ca40015f5a3
Owen Jones <otj202@nyu.edu>
2021-10-17 17:23:56 -0400

    printing redis url

--- tasks.py
+++ tasks.py
@@ -2,13 +2,10 @@ from celery import Celery
 import ai_model
 import chess
 import os
-import ssl
+
 REDIS_URL=os.getenv("REDIS_URL") + "?ssl_cert_reqs=none"
-celery_app = None
-try:
-	celery_app=Celery('tasks',backend=REDIS_URL,broker=REDIS_URL)
-except:
-	print("/n/n/n/n/n url is ",REDIS_URL)
+print("REDIS_URL IS ",REDIS_URL)
+celery_app=Celery('tasks',backend=REDIS_URL,broker=REDIS_URL)
 celery_app.conf.update(broker_pool_limit=None,broker_url=REDIS_URL,result_backend=REDIS_URL)
 @celery_app.task
 def generate_machine_move(fen):
